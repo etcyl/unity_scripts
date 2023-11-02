09f3f93070516148305f37de9c734ea299838dba
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2023-11-02 14:48:17 -0700

    Add files via upload
    A basic JSON file template for storing and saving player data.

---
+++ Scripts/PlayerProfile.cs
@@ -0,0 +1,26 @@
+[System.Serializable]
+public class PlayerProfile
+{
+    public string playerName;
+    public float health;
+    public float maxhealth;
+    public float maxmana;
+    public float mana;
+    public float manaRegen;
+    public float healthRegen;
+    public float agility;
+    public float strength;
+    public float magic;
+    public float defense;
+    public float defenseMitigation;
+    public float level;
+    public float experience;
+    public string playerClass;
+    public string profilePath;
+    public int profileIndex;
+    public int numReusableHealthPots;
+    public int numReusableManaPots;
+    public int coins;
+    public string timePlayed;
+    public int enemiesKilled;
+}
